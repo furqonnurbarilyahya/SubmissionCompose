8553d2ae06bc18cbe48aadc7346c2b0188bce070
Furqon Nurbaril Yahya <furqonyahya1@gmail.com>
2023-11-15 09:00:01 +0700

    update ImageUrl

--- app/src/main/java/com/bangkit/submissioncompose/model/FootballPlayersData.kt
+++ app/src/main/java/com/bangkit/submissioncompose/model/FootballPlayersData.kt
@@ -5,102 +5,102 @@ object FootballPlayersData {
         FootbalPlayer(
             "1",
             "Wayne Rooney",
-            "https://en.wikipedia.org/wiki/File:Wayne-Rooney-2015-10-21.jpg"
+            "https://upload.wikimedia.org/wikipedia/commons/0/0c/Wayne-Rooney-2015-10-21.jpg"
         ),
         FootbalPlayer(
             "2",
             "Cristiano Ronaldo",
-            "https://en.wikipedia.org/wiki/File:C_Ronaldo.jpg"
+            "https://ojbsport.files.wordpress.com/2021/08/img_8607.jpg?w=960"
         ),
         FootbalPlayer(
             "3",
             "Paul Scholes",
-            "https://en.wikipedia.org/wiki/File:Paul_Scholes_2008.jpg"
+            "https://upload.wikimedia.org/wikipedia/commons/b/b4/Paul_Scholes_2008.jpg"
         ),
         FootbalPlayer(
             "4",
             "Eric Cantona",
-            "https://en.wikipedia.org/wiki/File:Eric_Cantona_Cannes_2009.jpg"
+            "https://upload.wikimedia.org/wikipedia/commons/f/f4/Eric_Cantona_Cannes_2009.jpg"
         ),
         FootbalPlayer(
             "5",
             "Nemanja Vidic",
-            "https://en.wikipedia.org/wiki/File:Nemanja_Vidi%C4%87.jpg"
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Nemanja_Vidi%C4%87.jpg/737px-Nemanja_Vidi%C4%87.jpg"
         ),
         FootbalPlayer(
             "6",
             "Robin Van Persie",
-            "https://en.wikipedia.org/wiki/File:Manchester_United_(8400990621)_(cropped).jpg"
+            "https://upload.wikimedia.org/wikipedia/commons/0/03/Manchester_United_%288400990621%29_%28cropped%29.jpg"
         ),
         FootbalPlayer(
             "7",
             "George Best",
-            "https://en.wikipedia.org/wiki/File:George_Best_(1976).jpg"
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/George_Best_%281976%29.jpg/713px-George_Best_%281976%29.jpg"
         ),
         FootbalPlayer(
             "8",
             "Bobby Chartlon",
-            "https://en.wikipedia.org/wiki/File:Bobby_Charlon_(1970).jpg"
+            "https://upload.wikimedia.org/wikipedia/commons/0/0b/Bobby_Charlon_%281970%29.jpg"
         ),
         FootbalPlayer(
             "9",
             "Tommy Taylor",
-            "https://en.wikipedia.org/wiki/File:Tommy_Taylor_1957_(cropped).jpg"
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Tommy_Taylor_1957_%28cropped%29.jpg/710px-Tommy_Taylor_1957_%28cropped%29.jpg"
         ),
         FootbalPlayer(
             "10",
             "Jack Rowley",
-            "https://en.wikipedia.org/wiki/File:Jack_Rowley.jpg"
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Jack_Rowley.jpg/681px-Jack_Rowley.jpg"
         ),
         FootbalPlayer(
             "11",
-            "Billy Meredith",
-            "https://en.wikipedia.org/wiki/File:Billymeredith.jpg"
+            "Ryan Giggs",
+            "https://cdn1-production-images-kly.akamaized.net/j9JJgScuIhJacQNzbAQQLt16rCE=/800x450/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3099453/original/007500100_1586592173-000_Par3196931.jpg"
         ),
         FootbalPlayer(
             "12",
             "Ole Gunnar Solksjaer",
-            "https://en.wikipedia.org/wiki/File:Ole_G_Solskjaer.jpg"
+            "https://assets.manutd.com/AssetPicker/images/0/0/10/126/687724/Legends-Profile_Ole-Gunnar-Solskjaer1523463504754.jpg"
         ),
         FootbalPlayer(
             "13",
             "David Beckham",
-            "https://en.wikipedia.org/wiki/File:David_Beckham_Nov_11_2007.jpg"
+            "https://upload.wikimedia.org/wikipedia/commons/7/7b/David_Beckham_Nov_11_2007.jpg"
         ),
         FootbalPlayer(
             "14",
             "Peter Smeichel",
-            "https://en.wikipedia.org/wiki/File:Peter_Schmeichel_juli_1991.JPG"
+            "https://upload.wikimedia.org/wikipedia/commons/0/02/Peter_Schmeichel_juli_1991.JPG"
         ),
         FootbalPlayer(
             "15",
             "Rio Ferdinand",
-            "https://en.wikipedia.org/wiki/File:Rio_Ferdinand.jpg"
+            "https://images.hindustantimes.com/rf/image_size_640x362/HT/p1/2013/05/15/Incoming/Pictures/1060418_Wallpaper2.jpg"
         ),
         FootbalPlayer(
             "16",
             "Patrice Evra",
-            "https://en.wikipedia.org/wiki/File:Patriceevramls2011gane.jpg"
+            "https://upload.wikimedia.org/wikipedia/commons/d/da/Patriceevramls2011gane.jpg"
         ),
         FootbalPlayer(
             "17",
             "David de Gea",
-            "https://en.wikipedia.org/wiki/File:David_de_Gea_2017.jpg"
+            "https://tmssl.akamaized.net/images/foto/galerie/david-de-gea-manchester-united-1666004862-94382.jpg?lm=1666004877"
         ),
         FootbalPlayer(
             "18",
             "Juan Mata",
-            "https://en.wikipedia.org/wiki/File:Zarya-MU_(10).jpg"
+            "https://setanmerah.net/wp-content/uploads/2022/06/GettyImages-1234164619.jpg"
         ),
         FootbalPlayer(
             "19",
             "Bruno Fernandes",
-            "https://en.wikipedia.org/wiki/File:Bruno_Fernandes_Portugal,_2018.jpg"
+            "https://static.independent.co.uk/2023/07/23/01/9033d4ce53d4e04d2026454b863ccc31Y29udGVudHNlYXJjaGFwaSwxNjkwMTU2OTU5-2.72435026.jpg"
         ),
         FootbalPlayer(
             "20",
             "Marcus Rashford",
-            "https://en.wikipedia.org/wiki/File:Press_Tren_CSKA_-_MU_(3).jpg"
+            "https://upload.wikimedia.org/wikipedia/commons/5/5e/Press_Tren_CSKA_-_MU_%283%29.jpg"
         ),
         FootbalPlayer(
             "21",
