c1424ed9bcf903406b7d3238bf766c390c89b01c
Furqon Nurbaril Yahya <furqonyahya1@gmail.com>
2023-11-15 12:31:27 +0700

    add menu about button

--- app/src/main/java/com/bangkit/submissioncompose/FootbalPlayersApp.kt
+++ app/src/main/java/com/bangkit/submissioncompose/FootbalPlayersApp.kt
@@ -12,21 +12,27 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -52,8 +58,10 @@ import com.bangkit.submissioncompose.ui.theme.SubmissionComposeTheme
 import kotlinx.coroutines.launch
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun FootballPlayersApp(
     modifier: Modifier = Modifier,
@@ -62,6 +70,18 @@ fun FootballPlayersApp(
     val groupedPlayers by viewModel.groupedPlayers.collectAsState()
     val query by viewModel.query
 
+//    Scaffold (
+//        topBar = {
+//            TopAppBar(
+//                title = {
+//                    Text(text = "Footbal Player App")
+//                },
+//            )
+//        }
+//
+//    ) {innerPadding ->
+//
+//    }
     Box (modifier = modifier) {
         val scope = rememberCoroutineScope()
         val listState = rememberLazyListState()
@@ -73,11 +93,33 @@ fun FootballPlayersApp(
             contentPadding = PaddingValues(bottom = 80.dp)
         ) {
             item {
-                SearchBar(
-                    query = query,
-                    onQueryChange = viewModel::search,
-                    modifier = Modifier.background(MaterialTheme.colorScheme.primary)
-                )
+                Row (
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.primary)
+                        .padding(all = 10.dp)
+                ) {
+                    SearchBar(
+                        query = query,
+                        onQueryChange = viewModel::search,
+                        modifier = Modifier
+                            .weight(1F, true)
+                    )
+                    IconButton(
+                        onClick = {
+
+                        },
+                        modifier = Modifier
+                            .align(CenterVertically),
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Person,
+                            contentDescription = "about_page",
+                            tint = Color.White,
+                            modifier = Modifier.size(30.dp, 30.dp),
+                        )
+                    }
+
+                }
             }
             groupedPlayers.forEach { (initial, players) ->
                 stickyHeader {
