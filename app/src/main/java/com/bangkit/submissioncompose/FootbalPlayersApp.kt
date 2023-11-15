bf22e2035b7224910535ade48eb499a68d3de3af
Furqon Nurbaril Yahya <furqonyahya1@gmail.com>
2023-11-15 08:59:39 +0700

    menampilkan list dan button scroll to top

--- app/src/main/java/com/bangkit/submissioncompose/FootbalPlayersApp.kt
+++ app/src/main/java/com/bangkit/submissioncompose/FootbalPlayersApp.kt
@@ -1,11 +1,162 @@
 package com.bangkit.submissioncompose
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.bangkit.submissioncompose.model.FootballPlayersData
+import com.bangkit.submissioncompose.ui.theme.SubmissionComposeTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun FootballPlayersApp(
     modifier: Modifier = Modifier
 ) {
+    val groupedHeroes = FootballPlayersData.players
+        .sortedBy { it.name }
+        .groupBy { it.name[0] }
 
+    Box (modifier = modifier) {
+        val scope = rememberCoroutineScope()
+        val listState = rememberLazyListState()
+        val showButton: Boolean by remember {
+            derivedStateOf { listState.firstVisibleItemIndex > 0 }
+        }
+        LazyColumn (
+            state = listState
+        ) {
+
+            items(FootballPlayersData.players, key = { it.id }) { player ->
+                PlayerListItem(
+                    name = player.name,
+                    photoUrl = player.photoUrl,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+        AnimatedVisibility(
+            visible = showButton,
+            enter = fadeIn() + slideInVertically(),
+            exit = fadeOut() + slideOutVertically(),
+            modifier = Modifier
+                .padding(bottom = 30.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            ScrollToTopButton(
+                onClick = {
+                    scope.launch {
+                        listState.scrollToItem(index = 0)
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun PlayerListItem(
+    name: String,
+    photoUrl: String,
+    modifier: Modifier = Modifier
+) {
+    Row (
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.clickable {  }
+    ) {
+        AsyncImage(
+            model = photoUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(65.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = name,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(start = 16.dp)
+        )
+    }
+}
+
+@Composable
+fun ScrollToTopButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    FilledIconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = Icons.Filled.KeyboardArrowUp,
+            contentDescription = stringResource(R.string.scroll_to_top)
+        )
+    }
+}
+
+//@Composable
+//fun CharacterHead(
+//    char: Char,
+//    modifier: Modifier = Modifier
+//) {
+//    Surface (
+//        color = MaterialTheme.colorScheme.primary,
+//        modifier = modifier
+//    ) {
+//        Text(
+//            text = char.toString(),
+//            fontWeight = FontWeight.Black,
+//            color = Color.White,
+//            textAlign = TextAlign.Center,
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(8.dp)
+//        )
+//    }
+//}
+
+@Preview(showBackground = true, showSystemUi = true, device = Devices.PIXEL_3A)
+@Composable
+fun FootballPlayersAppPrevie() {
+    SubmissionComposeTheme {
+        FootballPlayersApp()
+    }
 }
